b3661d45b717f9dabfcc642ad88c2b552c43963b
DanielleColucci <ddcolucci99@gmail.com>
2023-01-04 17:34:39 -0500

    add tempory skills data

---
+++ data/skill-data.js
@@ -0,0 +1,9 @@
+const skills = [
+  {text: 'JavaScript', mastered: true, _id: 123456},
+  {text: 'Algorithms', mastered: false, _id: 123457},
+  {text: 'Git', mastered: true, _id: 123458},
+]
+
+export {
+  skills
+}
